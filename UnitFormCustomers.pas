2851071e4b89b2ad8fdd9c80058f957d88cd5cde
Andy <smdeveloper@aol.com>
2011-09-30 00:36:18 +0100

    Still Name Refactoring And Form Splitting Articles, Customers, Invoices

---
+++ UnitFormCustomers.pas
@@ -0,0 +1,26 @@
+unit UnitFormCustomers;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TBMViewerCustomers = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  BMViewerCustomers: TBMViewerCustomers;
+
+implementation
+
+uses UnitMiscShared;
+
+{$R *.dfm}
+
+end.
